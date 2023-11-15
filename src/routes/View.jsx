e5de57552ebdf778c61e22ecbe6fbe99f4253000
waihlyanaung-lastoenjoy <wailyanaung.lastoenjoy@gmail.com>
2023-11-15 18:51:27 +0630

    v1

--- src/routes/View.jsx
+++ src/routes/View.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
+import Hello from "../pages/Hello";
 
 const View = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/hello" element={<Hello />} />
       </Routes>
     </>
   );
